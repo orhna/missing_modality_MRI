a6ede8dd8be834489cffe6b063527e0ccf872873
orhna <aorhunaksoy@gmail.com>
2025-07-12 21:17:28 +0200

    update

--- README.md
+++ README.md
@@ -1,16 +1,48 @@
-## ⚙️ Environment Setup
+# 🧠 Project Title
 
-We recommend using a **Conda environment** to manage dependencies.
+Brief project description here (what the repo does, key goals, etc.)
 
-### 🔧 Step 1: Clone the Repository
 
+## 📦 Installation
+
+### 1. Clone the repository
+### 2. Create new virtual environment
+### 3. Install required packages
 ```bash
+pip install -r requirements.txt
+```
+
 git clone https://github.com/yourusername/your-repo.git
 cd your-repo
 
+## 📦 Configuration
+Use `config.py` to set up dataset, datalist and model saving paths. For example:
 
-conda create -y -p $ENV_PATH python=$PYTHON_VERSION pip
-
-pip install -r requirements.txt
+`model_save_path = "./models/"`
 
 
+`img_path["BRATS18"] = "/data/Images" `
+`seg_path["BRATS18"] = "/data/Labels" `
+`split_path["BRATS18"]["train"] = "/datasplit/train.txt"` 
+`split_path["BRATS18"]["val"] = "/datasplit/val.txt"`
+`split_path["BRATS18"]["test"] = "/datasplit/test.txt"`
+
+## 📦 Training
+
+Use `config.py` to set up experiment name:
+`experiment_name = "your_experiment_name"`
+
+Set `recon_level` to `"hs3_hs4"` for training with learnable token method, or use `"none"` to train without explicit feature reconstruction method.
+
+Then run:
+`python train.py --device_id 0`
+
+And for joint memory training on two modality setting, run:
+`python train2.py --device_id 0`
+
+Logs will be saved under `/logs/your_experiment_name`
+
+
+## 🔍 Inference
+Set the testing parameters in `config.py` -> `Test_config`, then run:
+`python test.py --device_id 0`
--- config.py
+++ config.py
@@ -18,16 +18,15 @@ class Training_config:
         recon_level = "hs3_hs4" # {"hs3", "hs4", "hs3_hs4", "none"}
         use_memory = False 
         continue_training = True
-        checkpoint_path = "/data/hjlee/orhun/thesis/models/joint_hs3_hs4_5e-4/joint_hs3_hs4_5e-4_checkpoint_Epoch_400.pt"
+        checkpoint_path = "/models/experiment_name/experiment_name_checkpoint.pt"
         # model_save_path 
-        model_save_path = "/data/hjlee/orhun/thesis/models/"
+        model_save_path = "/models/"
         cropped_input_size = [128,128,128]   
         output_channel = 3
         workers = 2
 
 class Database_config:
         channels={}
-        # modalities for each database, the order in the pre-processed files from 0-3
         channels['BRATS18'] = ["FLAIR", "T1", "T1c", "T2"]
         channels['BRATS23'] = ["T1c", "T1", "FLAIR", "T2"]
 
@@ -37,17 +36,17 @@ class Database_config:
         split_path["BRATS18"]={}
         split_path["BRATS23"]={}
         
-        img_path["BRATS18"] = "/data/hjlee/orhun/data/BRATS18/BRATS18_Preprocessed_C/Training/Images" # "data/BRATS/Images"
-        seg_path["BRATS18"] = "/data/hjlee/orhun/data/BRATS18/BRATS18_Preprocessed_C/Training/Labels"
-        split_path["BRATS18"]["train"] = "/data/hjlee/orhun/data/BRATS18/BRATS18_Preprocessed/train_val.txt" # train_overfit, train_val,
-        split_path["BRATS18"]["val"] = "/data/hjlee/orhun/data/BRATS18/BRATS18_Preprocessed/val_test.txt" # train_overfit, val_test
-        split_path["BRATS18"]["test"] = "/data/hjlee/orhun/data/BRATS18/BRATS18_Preprocessed/test.txt"
+        img_path["BRATS18"] = "/data/Images" 
+        seg_path["BRATS18"] = "/data/Labels"
+        split_path["BRATS18"]["train"] = "/datasplit/train.txt" 
+        split_path["BRATS18"]["val"] = "/datasplit/val.txt" 
+        split_path["BRATS18"]["test"] = "/datasplit/test.txt"
 
-        img_path["BRATS23"] = "/data/hjlee/orhun/data/BRATS21_Processed/Training/Images" # "data/BRATS/Images"
-        seg_path["BRATS23"] = "/data/hjlee/orhun/data/BRATS21_Processed/Training/Labels"
-        split_path["BRATS23"]["train"] = "/data/hjlee/orhun/data/BRATS21_Processed/train.txt"
-        split_path["BRATS23"]["val"] = "/data/hjlee/orhun/data/BRATS21_Processed/test.txt"
-        split_path["BRATS23"]["test"] = "/data/hjlee/orhun/data/BRATS21_Processed/test.txt"
+        img_path["BRATS23"] = "/data/Images"
+        seg_path["BRATS23"] = "/data/Labels"
+        split_path["BRATS23"]["train"] = "/datasplit/train.txt"
+        split_path["BRATS23"]["val"] = "/datasplit/val.txt"
+        split_path["BRATS23"]["test"] = "/datasplit/test.txt"
 
 class Test_config:
         dataset = "BRATS18"
@@ -56,6 +55,5 @@ class Test_config:
         crop_size = 128
         n_of_output_c = 3
         feature_size = 12
-        model_file_path = "/data/hjlee/orhun/thesis/models/mm12_sd_ds_dicece_rd_evalall_updated/mm12_sd_ds_dicece_rd_evalall_updated_checkpoint_Epoch_420.pt"
+        model_file_path = "/models/experiment_name/experiment_name_checkpoint.pt"
         random_dropping = "modality_mean" # {"whole_mean","modality_mean", "pc_modality_mean", "zero"} 
-        pc_mean_path = "/mnt/disk1/hjlee/orhun/repo/thesis/pc_mean.pt"
